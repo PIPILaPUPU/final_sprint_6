262d22f525c3ec64c24f3f1d79f7bbc3fa2afbaa
MooDCAT <stepashetinin@gmail.com>
2025-08-21 10:17:49 +0300

    commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convertation(input string) string {
+	input = strings.TrimSpace(input)
+
+	if strings.ContainsAny(input, ".-") {
+		return morse.ToText(input)
+	}
+
+	return morse.ToMorse(input)
+}
